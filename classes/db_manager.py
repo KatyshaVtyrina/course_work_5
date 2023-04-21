30f210fb849ccf79b0d6ef6a963e8c8756491ae3
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-04-21 18:32:35 +0300

    correction init, insert, execute_query delete connect, disconnect add create_database

--- classes/db_manager.py
+++ classes/db_manager.py
@@ -1,77 +1,108 @@
 import psycopg2
-import psycopg2.errors
+from psycopg2 import errors
 
 
 class DBManager:
+    """Класс для работы с базой данных, инициализируется названием базы данных и данными из конфигурационного файла"""
+    def __init__(self, dbname: str, params: dict):
+        self.dbname = dbname
+        self.params = params
 
-    def __init__(self, host: str, database: str, user: str, password: str, port=5432):
-        self.host = host
-        self.database = database
-        self.user = user
-        self.port = port
-        self.password = password
-
-    def _connect(self) -> None:
-        """Подключение к базе данных"""
+    def create_database(self):
+        """Создает базу данных и таблицы"""
         try:
-            self.conn = psycopg2.connect(host=self.host,
-                                         database=self.database,
-                                         user=self.user,
-                                         password=self.password,
-                                         port=self.port)
-        except psycopg2.OperationalError:
-            print("Не удалось подключиться к базе данных")
+            # Подключаемся к postgres, чтобы создать БД
+            conn = psycopg2.connect(dbname='postgres', **self.params)
+            conn.autocommit = True
+            cur = conn.cursor()
 
-    def _disconnect(self) -> None:
-        """Отключение от базы данных"""
-        if self.conn:
-            self.conn.close()
+            cur.execute(f"DROP DATABASE IF EXISTS {self.dbname}")
+            cur.execute(f"CREATE DATABASE {self.dbname}")
+
+        except psycopg2.errors.ObjectInUse:
+            # Если было активное подключение, удаляет его, удаляет БД и создает ее заново
+            cur.execute("SELECT pg_terminate_backend(pg_stat_activity.pid) "
+                        "FROM pg_stat_activity "
+                        f"WHERE pg_stat_activity.datname = '{self.dbname}' ")
+            cur.execute(f"DROP DATABASE IF EXISTS {self.dbname}")
+            cur.execute(f"CREATE DATABASE {self.dbname}")
+            print('Было активное подключение к базе данных, мы его удалили')
+            print('................................')
+
+        finally:
+            cur.close()
+            conn.close()
+
+        try:
+            # Подключаемся к созданной БД и создаем таблицы
+            conn = psycopg2.connect(dbname=self.dbname, **self.params)
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute('CREATE TABLE IF NOT EXISTS employers '
+                                '('
+                                'employer_id int PRIMARY KEY, '
+                                'employer_name varchar(255) UNIQUE NOT NULL)')
+
+                    cur.execute('CREATE TABLE IF NOT EXISTS vacancies '
+                                '('
+                                'vacancy_id int PRIMARY KEY, '
+                                'vacancy_name varchar(255) NOT NULL, '
+                                'employer_id int REFERENCES employers(employer_id) NOT NULL, '
+                                'city varchar(255), '
+                                'url text, '
+                                'salary int)')
+        finally:
+            conn.close()
 
     def insert(self, table: str, data: list) -> None:
         """Добавление данных в базу данных в зависимости от таблицы"""
+
         try:
-            self._connect()
-            with self.conn:
-                with self.conn.cursor() as cur:
+            conn = psycopg2.connect(dbname=self.dbname, **self.params)
+            with conn:
+                with conn.cursor() as cur:
                     if table == 'employers':
                         cur.executemany('INSERT INTO employers(employer_id, employer_name) '
                                         'VALUES(%s, %s)', data)
                     elif table == 'vacancies':
-                        cur.executemany('INSERT INTO vacancies(vacancy_id, vacancy_name, employer_name, salary, url) '
-                                        'VALUES(%s, %s, %s, %s, %s)', data)
-
-        except psycopg2.errors.UniqueViolation:
-            print("Уже есть в таблице")
+                        cur.executemany('INSERT INTO vacancies (vacancy_id, vacancy_name, employer_id, '
+                                        'city, salary, url) '
+                                        'VALUES(%s, %s, %s, %s, %s, %s)'
+                                        'ON CONFLICT (vacancy_id) DO NOTHING', data)
         finally:
-            self._disconnect()
+            conn.close()
 
     def _execute_query(self, query) -> list:
         """Возвращает результат запроса"""
         try:
-            self._connect()
-            with self.conn:
-                with self.conn.cursor() as cur:
+            conn = psycopg2.connect(dbname=self.dbname, **self.params)
+            with conn:
+                with conn.cursor() as cur:
                     cur.execute(query)
                     result = cur.fetchall()
+
         finally:
-            self._disconnect()
+            conn.close()
+
         return result
 
     def get_companies_and_vacancies_count(self):
         """Получает список всех компаний и количество вакансий у каждой компании"""
-        result = self._execute_query("SELECT employer_name, COUNT(*) "
+        result = self._execute_query("SELECT employer_name, COUNT(*) as quantity_vacancies "
                                      "FROM vacancies "
+                                     "LEFT JOIN employers USING(employer_id)"
                                      "GROUP BY employer_name "
-                                     "ORDER BY COUNT(*) DESC")
+                                     "ORDER BY quantity_vacancies DESC, employer_name")
         return result
 
     def get_all_vacancies(self):
         """ Получает список всех вакансий с указанием названия компании,
         названия вакансии и зарплаты и ссылки на вакансию"""
-        result = self._execute_query("SELECT employer_name, vacancy_name, salary, url "
+        result = self._execute_query("SELECT employers.employer_name, vacancy_name, salary, url "
                                      "FROM vacancies "
+                                     "JOIN employers USING(employer_id)"
                                      "WHERE salary IS NOT NULL "
-                                     "ORDER BY salary DESC")
+                                     "ORDER BY salary DESC, vacancy_name")
         return result
 
     def get_avg_salary(self):
@@ -82,14 +113,16 @@ class DBManager:
 
     def get_vacancies_with_higher_salary(self):
         """ Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
-        result = self._execute_query("SELECT vacancy_name "
+        result = self._execute_query("SELECT vacancy_name, salary "
                                      "FROM vacancies "
-                                     "WHERE salary > (SELECT AVG(salary) FROM vacancies)")
+                                     "WHERE salary > (SELECT AVG(salary) FROM vacancies) "
+                                     "ORDER BY salary DESC, vacancy_name")
         return result
 
-    def get_vacancies_with_keyword(self, word) -> list:
+    def get_vacancies_with_keyword(self, word: str) -> list:
         """Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”"""
         result = self._execute_query("SELECT vacancy_name "
                                      "FROM vacancies "
-                                     f"WHERE vacancy_name LIKE '%{word}%'")
+                                     f"WHERE vacancy_name ILIKE '%{word}%'"
+                                     "ORDER BY vacancy_name")
         return result
